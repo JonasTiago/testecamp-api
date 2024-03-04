29a1324fd4266e6d15cd73604a3370343b43fd75
JonasTiago <jonastiago95@gmail.com>
2024-03-04 10:49:53 -0300

    first commit - aula 03 java

---
+++ src/test/java/com/testecamp/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testecamp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
